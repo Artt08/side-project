778d42ca5cf6316010549c291034d9e15ffc50d1
That.cringe.Guy <152317110+Artt08@users.noreply.github.com>
2023-12-21 22:13:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 
-    function checkTextLength() {
-        // Get the value from the text box
-        var enteredText = document.getElementById('textInput').value;
+    function checkWord() {
+            var userInput = document.getElementById('wordInput').value.toLowerCase();
 
-        // Check if the length of the entered text is seven
-        var isSevenLetters = enteredText.length === 7;
-
-        // Display the result
-        var resultMessage = isSevenLetters ? 'true' : 'false';
-        document.getElementById('result').innerHTML = resultMessage;
-    }
+            if (userInput === 'tales') {
+                // If the entered word is 'tales'
+                document.getElementById('result').innerHTML = '<a href="https://example.com"><img src="https://example.com/your-image.jpg" alt="Tales Image"></a>';
+            } else {
+                // If the entered word is not 'tales'
+                document.getElementById('result').innerText = 'Try again!';
+            }
+        }
