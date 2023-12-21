85db42936434c978e6d8410a8e36c9515bfa3684
That.cringe.Guy <152317110+Artt08@users.noreply.github.com>
2023-12-22 00:50:49 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
 
     function checkWord() {
-            var userInput = document.getElementById('wordInput').value.toLowerCase();
+        var userInput = document.getElementById('wordInput').value.toLowerCase();
 
-            if (userInput === 'tales') {
-                // If the entered word is 'tales'
-                document.getElementById('result').innerHTML = '<a href="https://example.com"><img src="https://example.com/your-image.jpg" alt="Tales Image"></a>';
-            } else {
-                // If the entered word is not 'tales'
-                document.getElementById('result').innerText = 'Try again!';
-            }
+        if (userInput === 'tales') {
+            // If the entered word is 'tales'
+            document.getElementById('result').innerHTML = '<a href="https://example.com"><img src="https://cdn.discordapp.com/attachments/940583485964431431/1186690989453213736/1048206470640050276.png?ex=65942b1b&is=6581b61b&hm=27c62a9d9ebb4c257e536e1923b5f2b12e8382f1d467ec2dfcac985eab462a9e&" alt="Tales Image"></a>';
+        } else {
+            // If the entered word is not 'tales'
+            var randomMessages = [
+                'Try again!',
+                'Not quite there.',
+                'Give it another shot.',
+                'Oops, that\'s not it.',
+                'Keep trying!'
+            ];
+
+            var randomIndex = Math.floor(Math.random() * randomMessages.length);
+            document.getElementById('result').innerText = randomMessages[randomIndex];
         }
+    }
