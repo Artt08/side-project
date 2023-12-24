413d802ff91b13399db3b2464f9483779878d895
That.cringe.Guy <152317110+Artt08@users.noreply.github.com>
2023-12-24 19:48:20 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
 
         if (userInput === 'tales') {
             // If the entered word is 'tales'
-            document.getElementById('result').innerHTML = '<a href="https://discord.gg/rakazone-s-chill-zone-292718713453412362"><img src="https://cdn.discordapp.com/attachments/940583485964431431/1186690989453213736/1048206470640050276.png?ex=65942b1b&is=6581b61b&hm=27c62a9d9ebb4c257e536e1923b5f2b12e8382f1d467ec2dfcac985eab462a9e&" alt="Tales Image"></a>';
+            document.getElementById('result').innerHTML = '<a href="https://discord.com/channels/292718713453412362/874645597301391440"><img src="https://cdn.discordapp.com/attachments/940583485964431431/1186690989453213736/1048206470640050276.png?ex=65942b1b&is=6581b61b&hm=27c62a9d9ebb4c257e536e1923b5f2b12e8382f1d467ec2dfcac985eab462a9e&" alt="Tales Image"></a>';
         } else {
             // If the entered word is not 'tales'
             var randomMessages = [
