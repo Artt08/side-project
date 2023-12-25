67837865c600fba196440a241bb6c57dd08836d9
That.cringe.Guy <152317110+Artt08@users.noreply.github.com>
2023-12-26 00:29:43 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,13 @@
-
-function checkWord() {
-  var userInput = document.getElementById('wordInput').value.toLowerCase();
-
-  if (userInput === 'tales') {
-    // If the entered word is 'tales'
-    document.getElementById('result').innerHTML = '<a href="https://discord.gg/rakazone-s-chill-zone-292718713453412362"><img src="final.png" alt="Tales Image"></a>';
+document.getElementById('toggle-btn').addEventListener('click', function () {
+  var sidebar = document.getElementById('sidebar');
+  var content = document.getElementById('content');
+  if (sidebar.style.left === '0px') {
+    sidebar.style.left = '-200px';
+    content.style.marginLeft = '0';
+    this.innerHTML = '≡';
   } else {
-    // If the entered word is not 'tales'
-    var randomMessages = [
-      'Try again!',
-      'Not quite there.',
-      'Give it another shot.',
-      'Oops, that\'s not it.',
-      'Keep trying!'
-    ];
-
-    var randomIndex = Math.floor(Math.random() * randomMessages.length);
-    document.getElementById('result').innerText = randomMessages[randomIndex];
+    sidebar.style.left = '0px';
+    content.style.marginLeft = '200px';
+    this.innerHTML = '≡';
   }
-}
+});
